55deccff441d8677dd25e3db40f912ae9a54a2b7
Neveu <nneveu@hawk.iit.edu>
2022-10-14 16:31:16 -0700

    small libe updates

--- lhs/sc_latin_sample.py
+++ lhs/sc_latin_sample.py
@@ -28,8 +28,8 @@ from libensemble.tools import parse_args, save_libE_output, add_unique_random_st
 from libensemble.message_numbers import WORKER_DONE, WORKER_KILL, TASK_FAILED, WORKER_KILL_ON_TIMEOUT
 from libensemble.executors.mpi_executor import MPIExecutor
 
-from libensemble import libE_logger
-libE_logger.set_level('DEBUG')
+from libensemble import logger
+logger.set_level('DEBUG')
 nworkers, is_master, libE_specs, _ = parse_args()
 libE_specs['save_every_k_gens'] = 100 
 libE_specs['save_every_k_sims'] = 1
@@ -48,7 +48,7 @@ FMAP_DIR = TOP_DIR + 'fieldmaps'
 
 # Register simulation executable with executor
 sim_app = '/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/opal'
-exctr.register_calc(full_path=sim_app, calc_type='sim')
+exctr.register_app(full_path=sim_app, calc_type='sim')
 
 libE_specs['comms'] = 'local'
 libE_specs['sim_dir_symlink_files'] = [ f for f in glob.glob(FMAP_DIR+'/*.txt')]
