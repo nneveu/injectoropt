5ef6ab2f40fb4fb2604e7617c04bf130973b07cf
Neveu <nneveu@hawk.iit.edu>
2022-10-17 17:32:23 -0700

    slurm working now?

--- lhs/slurm.sh
+++ lhs/slurm.sh
@@ -6,33 +6,38 @@
 #SBATCH --output=output-%j.txt
 #SBATCH --error=output-%j.txt
 #
-#SBATCH --nodes 2 
-#SBATCH --time=00:30:00
+#SBATCH -N 1 
+####SBATCH --exclusive
+
+######SBATCH --ntasks=9
+######SBATCH --cpus-per-task=5
+#####SBATCH --mem-per-cpu=1g
+#
+#SBATCH --time=00:40:00
 
 module remove openmpi
 module load devtoolset/9
 module list
 
-# Export SLURM_EXACT because of the new behavior in 21.08
-export SLURM_EXACT=1
-#export SLURM_MEM_PER_NODE=0
-export NUM_WORKERS=4
-
 source ~/.bashrc
-export PYTHONPATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/injectoropt/:$PYTHONPATH
+export PYTHONPATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/emittance_minimization/code/:$PYTHONPATH
 export PYTHONPATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/libensemble/:$PYTHONPATH
 export PYTHONPATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/distgen/:$PYTHONPATH
-export PATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/:$PATH
 
 conda activate
 conda activate /gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/libe
 
-which mpiexec
-#python latin_sample_sc.py --comms local --nworkers 9
-#/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/mpiexec -n 9 python latin_sample_sc.py
-#python call_vtmop_lhs_restart.py --comms local --nworkers 9
-mpiexec -np $(($NUM_WORKERS+1)) -ppn $(($NUM_WORKERS+1)) python latin_sample_sc.py
+export PATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/:$PATH
 
+# Export SLURM_EXACT because of the new behavior in 21.08
+export SLURM_EXACT=1
+#export SRUN_CPUS_PER_TASK=5
+#export SLURM_MEM_PER_NODE=0
+
+which mpirun
+#/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/mpiexec -n 5 python call_aposmm.py
+python latin_sample_sc.py --comms local --nworkers 7 
+#srun --ntasks=$(($NUM_WORKERS+1)) --nodes=1 python $EXE
 
 #
 # Print the date again -- when finished
